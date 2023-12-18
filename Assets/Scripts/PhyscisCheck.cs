f9dd59a44303d550098301e21b70ddd33eeb5e78
EnoshLi <1530217461@qq.com>
2023-12-19 03:26:29 +0800

    完成物理环境监测及绘制和部分文件未提交

---
+++ Assets/Scripts/PhyscisCheck.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhyscisCheck : MonoBehaviour
+{
+    [Header("基本参数")] 
+    public bool isGround;
+
+    public Vector2 bottomOffset;
+
+    public float radius;
+
+    public LayerMask layerMask;
+    private void Update()
+    {
+        Check();
+    }
+
+    public void Check()
+    {
+        isGround = Physics2D.OverlapCircle((Vector2)transform.position+bottomOffset,radius,layerMask);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere((Vector2)transform.position+bottomOffset,radius);
+    }
+}
