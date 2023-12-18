a0a47002e469f965217429dbb8a7018c19e7465e
EnoshLi <1530217461@qq.com>
2023-12-19 02:23:43 +0800

    完成人物的翻转和移动，删除了素材多余的空文件meta信息

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,15 +9,41 @@ public class PlayerController : MonoBehaviour
 {
     public PlayerInputControl playerInputControl;
     public Vector2 playerDirction;
+    public float speed;
+    public Rigidbody2D rb;
 
     private void Awake()
     {
         playerInputControl = new PlayerInputControl();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         playerDirction = playerInputControl.Player.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    //移动
+    private void Move()
+    {
+        rb.velocity = new Vector2(playerDirction.x*speed*Time.deltaTime,rb.velocity.y);
+        //人物翻转
+        int faceDir = (int)transform.localScale.x;
+        if (playerDirction.x>0)
+        {
+            faceDir = 1;
+        }
+
+        if (playerDirction.x<0)
+        {
+            faceDir = -1;
+        }
+
+        transform.localScale = new Vector3(faceDir,1,1);
     }
 
     private void OnEnable()
@@ -29,4 +55,5 @@ public class PlayerController : MonoBehaviour
     {
         playerInputControl.Disable();
     }
+    
 }
