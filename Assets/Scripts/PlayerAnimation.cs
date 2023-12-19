8ceff68d808b6c7d9bf28ab3d3a755fbe90f65f9
EnoshLi <1530217461@qq.com>
2023-12-20 00:19:46 +0800

    完成人物待机和运动动画

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    private void Update()
+    {
+        SetAnimation();
+    }
+
+    public void SetAnimation()
+    {
+        animator.SetFloat("velocityX",Mathf.Abs(rb.velocity.x));
+    }
+}
