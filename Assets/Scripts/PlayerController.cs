3ca30aaab38dd1f226947413c9dd41e4f58020fd
EnoshLi <1530217461@qq.com>
2023-12-21 01:10:33 +0800

    完成人物的下蹲动作和学习了Animation的次级动画

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,14 +11,23 @@ public class PlayerController : MonoBehaviour
     public Vector2 playerDirction;
     public Rigidbody2D rb;
     public PhyscisCheck physcisCheck;
+    private CapsuleCollider2D capsule2D;
+    
     [Header("基本参数")]
     public float speed;
     public float jumpForce;
+    public bool isCrouch;
+    private Vector2 offset;
+    private Vector2 size;
+    
     private void Awake()
     {
         playerInputControl = new PlayerInputControl();
         rb = GetComponent<Rigidbody2D>();
         physcisCheck = GetComponent<PhyscisCheck>();
+        capsule2D = GetComponent<CapsuleCollider2D>();
+        offset=capsule2D.offset;
+        size = capsule2D.size;
     }
 
     private void Update()
@@ -60,7 +69,12 @@ public class PlayerController : MonoBehaviour
     //移动
     private void Move()
     {
-        rb.velocity = new Vector2(playerDirction.x*speed*Time.deltaTime,rb.velocity.y);
+        //人物移动
+        if (!isCrouch)
+        {
+            rb.velocity = new Vector2(playerDirction.x * speed * Time.deltaTime, rb.velocity.y);
+        }
+
         //人物翻转
         int faceDir = (int)transform.localScale.x;
         if (playerDirction.x>0)
@@ -74,6 +88,18 @@ public class PlayerController : MonoBehaviour
         }
 
         transform.localScale = new Vector3(faceDir,1,1);
+        //下蹲
+        isCrouch = playerDirction.y < -0.5f && physcisCheck.isGround;
+        if (isCrouch)
+        {
+            capsule2D.offset= new Vector2(-0.05f,0.8f);
+            capsule2D.size = new Vector2(0.66f,1.6f);
+        }
+        else
+        {
+            capsule2D.offset = offset;
+            capsule2D.size = size;
+        }
     }
     
 }
