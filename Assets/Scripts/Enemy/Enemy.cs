7c196513ba2365be3cec38eac8e539fc13b2ce47
EnoshLi <1530217461@qq.com>
2024-01-03 22:38:31 +0800

    野猪 - 撞墙判定和等候计时

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -10,6 +10,8 @@ public class Enemy : MonoBehaviour
 
     protected Rigidbody2D rigidbody2D;
 
+    public PhyscisCheck physcisCheck;
+
     [Header("基本参数")]
     
     public float normalSpeed;
@@ -19,24 +21,28 @@ public class Enemy : MonoBehaviour
     public float ChaseSpeed;
     //面朝方向
     public Vector3 faceDir;
+    [Header("计时器")]
+    public bool wait;
+    public float waitTime;
+    public float waitCount;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
         rigidbody2D = GetComponent<Rigidbody2D>();
         currentSpeed = normalSpeed;
+        physcisCheck = GetComponent<PhyscisCheck>();
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         faceDir = new Vector3(-transform.localScale.x, 0, 0);
+        if ((physcisCheck.touchLeftWall && faceDir.x < 0 )|| (physcisCheck.touchRightWall && faceDir.x > 0))
+        {
+            wait = true;
+            animator.SetBool("Walk" ,false);
+        }
+        TimeCount();
     }
 
     private void FixedUpdate()
@@ -44,8 +50,31 @@ public class Enemy : MonoBehaviour
         Move();
     }
 
+    #region 移动
+
     public virtual void Move()
     {
         rigidbody2D.velocity = new Vector2(faceDir.x*currentSpeed * Time.deltaTime, rigidbody2D.velocity.y);
     }
+
+    #endregion
+
+    #region 计时器
+    public void TimeCount()
+    {
+        if (wait)
+        {
+            waitCount -= Time.deltaTime;
+            if (waitCount<=0)
+            {
+                wait = false;
+                waitCount = waitTime;
+                transform.localScale = new Vector3(faceDir.x, 1, 1);
+            }
+        }
+
+        
+    }
+    #endregion
+    
 }
--- Assets/Scripts/General/PhyscisCheck.cs
+++ Assets/Scripts/General/PhyscisCheck.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 
 public class PhyscisCheck : MonoBehaviour
 {
-    [Header("基本参数")] 
+    [Header("状态")]
     public bool isGround;
-
+    public bool touchRightWall;
+    public bool touchLeftWall;
+    [Header("基本参数")] 
     public Vector2 bottomOffset;
-
+    
+    public Vector2 rightOffset;
+    
+    public Vector2 leftOffset;
     public float radius;
 
     public LayerMask layerMask;
@@ -20,11 +25,17 @@ public class PhyscisCheck : MonoBehaviour
 
     public void Check()
     {
+        //地面检测
         isGround = Physics2D.OverlapCircle((Vector2)transform.position+bottomOffset,radius,layerMask);
+        //墙壁检测
+        touchRightWall=Physics2D.OverlapCircle((Vector2)transform.position+rightOffset,radius,layerMask);
+        touchLeftWall=Physics2D.OverlapCircle((Vector2)transform.position+leftOffset,radius,layerMask);
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere((Vector2)transform.position+bottomOffset,radius);
+        Gizmos.DrawWireSphere((Vector2)transform.position+rightOffset,radius);
+        Gizmos.DrawWireSphere((Vector2)transform.position+leftOffset,radius);
     }
 }
